37795b2caf9f2fb257e5840bec4996a957a79f97
Mink Wu <minkeiwu@gmail.com>
2024-11-17 11:38:17 +0800

    server up

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/watchdog", () => "I am running")
+  .get("/", () => "Hello Elysia, I am running").listen(process.env.PORT);
+  
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
